002b46fc9fc31f312f4a09c8ddbb5ab9cf943a9a
Ilya <ilundrik026@gmail.com>
2022-11-22 22:47:54 +0300

    add tabs

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
 // BURGER
 let burger = document.querySelector('.header-wrapper__hamburger');
-let menu = document.querySelector('.nav')
-let menuLinks = menu.querySelectorAll('.header-list__item')
+let menu = document.querySelector('.nav');
+let menuLinks = menu.querySelectorAll('.header-list__item');
 
-burger.addEventListener('click', function(){
+burger.addEventListener('click', function () {
   burger.classList.toggle('active');
   menu.classList.toggle('active');
   document.body.classList.toggle('stop-scroll');
-})
+});
 
-menuLinks.forEach(function(elem){
-  elem.addEventListener('click', function(){
+menuLinks.forEach(function (elem) {
+  elem.addEventListener('click', function () {
     burger.classList.remove('active');
     menu.classList.remove('active');
-    document.body.classList.remove('stop-scroll')
-  })
-})
+    document.body.classList.remove('stop-scroll');
+  });
+});
+
+// TABS
+let tabsBtn = document.querySelectorAll('.work-list__item');
+let tabsItem = document.querySelectorAll('.work-wrapper');
+
+tabsBtn.forEach(function (elem) {
+  elem.addEventListener('click', function(e){
+    const step = e.currentTarget.dataset.step;
+
+    tabsBtn.forEach(function(btn){
+      btn.classList.remove('active');
+    });
+    e.currentTarget.classList.add('active');
+
+    tabsItem.forEach(function(elem){
+      elem.classList.remove('active');
+    });
+    document.querySelector(`[data-work="${step}"]`).classList.add('active');
+  });
+});
